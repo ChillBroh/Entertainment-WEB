81968d4b75af357ede66cded84f5f0d47d271c6c
ChillBroh <isharamadusanka410@gmail.com>
2024-03-29 12:26:45 +0530

    frontend setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Loging from "./pages/Loging";
+import ResetPassword from "./pages/ResetPassword";
+import Registration from "./pages/Registration";
+import EmailConfirmation from "./pages/EmailConfirmation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/loging" element={<Loging />}></Route>
+          <Route path="/signup" element={<Registration />}></Route>
+          <Route path="/reset-password" element={<ResetPassword />}></Route>
+          <Route
+            path="/email-confirmation"
+            element={<EmailConfirmation />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
