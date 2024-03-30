5a40bb1561b53d503d6379b07428c5dc1e430f2b
ChillBroh <isharamadusanka410@gmail.com>
2024-03-30 19:04:03 +0530

    home and logins created:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,30 @@
-import "./App.css";
 import { React } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
-import Loging from "./pages/Loging";
+import Login from "./pages/Login";
 import ResetPassword from "./pages/ResetPassword";
 import Registration from "./pages/Registration";
 import EmailConfirmation from "./pages/EmailConfirmation";
+import LoginFormAttendee from "./pages/LoginFormAttendee";
+import LoginFormOrganizer from "./pages/LoginFormOrganizer";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/loging" element={<Loging />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<Registration />}></Route>
           <Route path="/reset-password" element={<ResetPassword />}></Route>
           <Route
             path="/email-confirmation"
             element={<EmailConfirmation />}
           ></Route>
+          <Route path="/login-attendee" element={<LoginFormAttendee />} />
+          <Route path="/login-organizer" element={<LoginFormOrganizer />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/LoginFormAttendee.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import bg from "../Components/Assets/dj-playing-and-mixing-music-in-nightclub-party-at-night-edm-dance-music-club-with-crowd-of-young-people-free-photo.jpg";
+import { Form, Input } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios";
+
+const LoginFormAttendee = () => {
+  const navigate = useNavigate;
+  const onFinish = async (values) => {
+    try {
+      const res = await axios.post("http://localhost:5000/api/v1/auth/login", {
+        email: values.email,
+        password: values.password,
+      });
+      console.log(res.data);
+
+      // if (res.data.isAdmin === true) {
+      //   navigate("/admin");
+      // }
+      // if (res.data.isAdmin === false) {
+      //   navigate("/");
+      // }
+    } catch (err) {
+      const res = err.response.status === 401 || 15;
+      if (res) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Invalid Email or Password",
+        });
+      }
+    }
+  };
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+      <div className="md:flex md:justify-end">
+        <img
+          src={bg}
+          alt="login dj"
+          className="object-cover h-full w-full md:max-h-full md:w-auto"
+        />
+      </div>
+      <div className="flex flex-col mt-24 px-12 md:pt-10 lg:pt-0 lg:px-20">
+        <div className="text-center mb-16">
+          <h1 className="text-5xl">Login</h1>
+        </div>
+        <div className="mt-16">
+          <Form name="login" onFinish={onFinish} autoComplete="off">
+            <h1 className="text-md mb-2">Email</h1>
+            <Form.Item
+              name="Email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input
+                placeholder="email"
+                className="w-full rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+            </Form.Item>
+            <h1 className="text-md mb-2">Password</h1>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password
+                placeholder="Password"
+                className="w-full rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+            </Form.Item>
+            <Link
+              to={"/reset-password"}
+              className="text-[#991AAD] hover:text-[#C7ADCE]"
+            >
+              Forgot Password
+            </Link>
+            <div>
+              <button
+                type="submit"
+                className="rounded-lg mt-16 bg-[#562595] pt-1 pb-1 w-full text-md font-medium uppercase text-neutral-50"
+                data-twe-ripple-init
+                data-twe-ripple-color="light"
+              >
+                Log in
+              </button>
+              <Link
+                to={"/signup"}
+                className="text-[#991AAD] hover:text-[#C7ADCE]"
+              >
+                <h1 className=" text-center mt-2 ">
+                  Don't Have an account? Sign Up
+                </h1>
+              </Link>
+            </div>
+          </Form>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginFormAttendee;
